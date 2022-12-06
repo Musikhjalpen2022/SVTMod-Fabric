3ed11a16c66d23eeb6a88f8a45054a736ef332d3
Rasmus Winzell <raswi727@student.liu.se>
2022-12-06 15:44:52 +0100

    Changed download site to Azure

--- src/main/java/com/cinemamod/downloader/Resource.java
+++ src/main/java/com/cinemamod/downloader/Resource.java
@@ -2,7 +2,7 @@ package com.cinemamod.downloader;
 
 public final class Resource {
 
-    public static final String BASE_URL = "http://129.151.206.9:8000/";
+    public static final String BASE_URL = "https://minecraftbibliotek.blob.core.windows.net/minecraft/jcef/";
     //public static final String BASE_URL = "https://cinemamod-libraries.ewr1.vultrobjects.com/";
     public static final String CINEMAMOD_VERSIONS_URL = BASE_URL + "versions.txt";
     public static final String CINEMAMOD_JCEF_URL_FORMAT = BASE_URL + "jcef/%s/%s";
